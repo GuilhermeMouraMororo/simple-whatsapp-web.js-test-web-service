472b55848fd0524158db8122f11406979869f2a1
GuilhermeMouraMororo <guilhermemouramororo@gmail.com>
2025-10-30 18:25:25 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  whatsappClientId: {
+    type: String,
+    unique: true,
+    sparse: true
+  },
+  whatsappReady: {
+    type: Boolean,
+    default: false
+  },
+  firstLogin: {
+    type: Boolean,
+    default: true
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// Compare password method
+userSchema.methods.correctPassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
